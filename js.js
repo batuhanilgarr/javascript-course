368f88ad887b37d188984dde6ac8ad7a1cd12a95
batuhanilgarr <batuhan.ilgar@8bitiz.com>
2022-07-23 23:16:42 +0300

    #

--- js.js
+++ js.js
@@ -173,5 +173,19 @@ alert("Hello, " + capitalisedName)
 
 
 
+// Numbers
+
+// humanAge = (dogAge -2 ) x 4 + 21
+
+var dogAge = prompt('What is your dog age :')
+
+humanAge = (dogAge - 2) * 4 + 21
+
+alert('Your dog is ' + humanAge + ' years old in human years.')
+
+
+
+
+
 
 
