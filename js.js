989f07ec5f20fd4129e9b97b787e409933b0a300
batuhanilgarr <batuhan.ilgar@8bitiz.com>
2022-07-24 15:18:52 +0300

    #

--- js.js
+++ js.js
@@ -300,3 +300,47 @@ getMilk(5)
 
 
 
+// VKİ Hesaplama
+
+function bmiCalculator(weight, height) {
+  var bmi = weight / (height * height)
+
+  return Math.round(bmi)
+}
+
+var bmi = bmiCalculator(65, 1.8)
+console.log(bmi)
+
+
+
+
+
+
+
+
+
+
+
+
+// Love Score Game
+
+prompt('What is your name?')
+prompt('What is their name?')
+
+var loveScore = Math.random() * 100
+loveScore = Math.floor(loveScore) + 1 //1-100
+
+if (loveScore > 70) {
+  alert(
+    'Your love score is ' + loveScore + '%' + ' You love each other like Kanye loves Kanye.'
+  )
+} else {
+  alert('Your love score is ' + loveScore + '%')
+}
+
+
+
+
+
+
+
